2037eccda5d065dad06547ebefd8b624174c46fb
Shadi <floydanthebeast@gmail.com>
2021-05-18 14:29:30 +0300

    Finish task 13

---
+++ 13.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+	fopen("test", "w+b");
+	symlink("test", "0");
+	char linkName[3], nextLinkName[3];
+	int linkIndex = 0;
+	int recursionDepth = 1;
+
+	sprintf(linkName, "%d", linkIndex);
+	while (fopen(linkName, "r") != NULL) {
+		recursionDepth++;
+		sprintf(linkName, "%d", linkIndex);
+		sprintf(nextLinkName, "%d", ++linkIndex);
+		symlink(linkName, nextLinkName);
+		sprintf(linkName, "%d", linkIndex);
+	}
+
+	printf("Recursion depth is %d\n", recursionDepth);
+	return 0;
+}
