b5662c46ee94d7a44083686cee42c2eeb8813158
Shadi <floydanthebeast@gmail.com>
2021-05-18 14:59:36 +0300

    Fix task 13

--- 13.c
+++ 13.c
@@ -6,7 +6,7 @@ int main() {
 	symlink("test", "0");
 	char linkName[3], nextLinkName[3];
 	int linkIndex = 0;
-	int recursionDepth = 1;
+	int recursionDepth = 0;
 
 	sprintf(linkName, "%d", linkIndex);
 	while (fopen(linkName, "r") != NULL) {
@@ -15,6 +15,11 @@ int main() {
 		sprintf(nextLinkName, "%d", ++linkIndex);
 		symlink(linkName, nextLinkName);
 		sprintf(linkName, "%d", linkIndex);
+	}
+
+	for (int i = 0; i < recursionDepth; i++) {
+		sprintf(linkName, "%d", i);
+		remove(linkName);
 	}
 
 	printf("Recursion depth is %d\n", recursionDepth);
